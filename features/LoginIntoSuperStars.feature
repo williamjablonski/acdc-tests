e0d77b7fc42d991ab62e2b828a78455dc3a6d103
Mirna Silva <mirna.silva@macys.com>
2015-07-27 18:19:02 -0300

    Almost 100%

--- features/LoginIntoSuperStars.feature
+++ features/LoginIntoSuperStars.feature
@@ -1,15 +1,15 @@
 # 09/07/2015 - QA Trainning - SuperStars Automation
 # Mirna Silva
 
- Feature: Verify the SuperStars website functionalities
+ Feature: Basic User Authentication
 
  Scenario: Verify if the website is performing a login correctly
-   Given that I navigate to the website
-   And I click on "Sign in" button
-   Then I click on "Add account" button
-   And I enter "msilva@avenuecode.com" into input field having id "Email" there
+   Given that I navigate to SuperStarsHomePage
+   Then I click on "Sign in" button
+   And I click on "Add account" button
+   Then I enter "user1acdc@avenuecode.com" into input field having id "Email" there
    And I click on "Next" button
-   And I enter "mps@1389" into input field having id "Passwd" there
+   Then I enter "acdcuser1" into input field having id "Passwd" there
    When I click on "Log In" button
-   #And I wait for 10 seconds
-   #Then I should be logged in
+   And I wait for 10 seconds
+   Then I should be logged in
--- features/step_definitions/sign_in.rb
+++ features/step_definitions/sign_in.rb
@@ -1,31 +1,31 @@
 
 
-Given (/^that I navigate to the website$/) do
-	visit('http://staging­superstars.avenuecode.com')
+Given (/^that I navigate to (.+?)$/) do |page|
+	visit(page)
 end
 
 And (/^I click on "(.*?)" button$/) do |button|
 	case button
 		when 'Sign in'
-			on(SuperStars::HomePage) do
+			on(SuperStarsHomePage) do
 				if sign_in_element.visible?
 					sign_in_element.click
 				end
 			end
 		when 'Add account'
-			on(SuperStars::SignInPage) do
+			on(SignInPage) do
 				if add_account_button_element.visible?
 					add_account_button_element.click
 				end
 			end
 		when 'Next'
-			on(SuperStars::SignInPage) do
+			on(SignInPage) do
 				if next_button_element.visible?
 					next_button_element.click
 				end
 			end
 		when 'Log in' 
-			on(SuperStars::SignInPage) do
+			on(SignInPage) do
 				if sign_in_button_element.visible?
 					sign_in_button_element.click
 				end
@@ -38,21 +38,24 @@ And (/^I enter "(.*?)" into input field having id "(.*?)" there$/) do |value, fi
   selected_field = nil
   case field
 		when 'Email'
-			on(SuperStars::SignInPage) do
+			on(SignInPage) do
         selected_field = account_user_element
 			end
 		when 'Passwd'
-			on(SuperStars::SignInPage) do
+			on(SignInPage) do
         selected_field = account_password_element
       end
       selected_field.value = value if(selected_field.visible?)
       Log.instance.debug "Field '#{field}' filled with value: #{value}"
 	end
+end
 
-  And (/^I wait for \d? seconds$/) do |seconds|
-    sleep (seconds)
+When (/^I wait for (\d+) seconds$/) do |seconds|
+    @browser.driver.manage.timeout.implicit_wait = seconds
     Log.instance.debug "Waited #{seconds} seconds"
-  end
+end
 
 
+Then(/^I should be logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
 end
